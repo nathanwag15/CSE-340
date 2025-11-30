4b652454ef03c009669455c349d3ed3f022fafd5
nathanwag15 <natewagstaff21@gmail.com>
2025-11-29 19:35:03 -0700

    server side validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,15 +2,30 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/accountValidation')
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+// Process the login attempt
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 
 // Route to build registration
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 // Route to post registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 
 module.exports = router
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
+const regValidate = require('../utilities/accountValidation')
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
